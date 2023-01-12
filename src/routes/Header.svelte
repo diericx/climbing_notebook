018ff4359d9edf7433116dd30350ff9ad4c18d32
Zac Holland <zacharyholland@gmail.com>
2023-01-12 19:58:47 +0100

    removed one more image I missed

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,12 +5,6 @@
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
