ad0ac93293ba804f7a94a7c0e0eae50586a07e35
Zac Holland <zacharyholland@gmail.com>
2023-01-30 15:39:47 +0100

    front page info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p><a href="/recovery">Recovery Tracker</a></p>
+<br />
+
+<h1>Welcome to the Climbing Notebook</h1>
+<hr />
+
+<p>
+	<br />
+	This website is a no-fluff set of tools to help climbers with their training.
+	<br />
+	This is a <b>very early version</b> and is actively being worked on by myself only.
+	<br />
+	I am currently jobless and am willing to work on this nearly full time right now.
+	<br />
+	<br />
+	<b>If you want to help out</b> we can chat about what tooling you want added next.
+	<br />
+	zacharyholland[at]gmail[dot]com
+</p>
+
+<br />
+
+<h1>Current Features</h1>
+<hr />
+<br />
+
+<ul class="roman">
+	<li>
+		<h2><b>Recovery Tracker</b></h2>
+		Implementation of the recovery tracking too suggested by Bechtel and Manganiello in<i
+			>Unstoppale Force: Strength Training for Climbing (pg. 92).</i
+		>
+		<ul class="list-disc px-6">
+			<li>Each activity falls under <b>work</b> or <b>rest</b></li>
+			<li>
+				Short term work/recovery is calculated by subtracting rest from work and displayed there
+			</li>
+			<li>
+				Short term work is displayed on the front page dashboard to get a quick glance at what might
+				affect your performance today.
+			</li>
+		</ul>
+	</li>
+	<li>
+		<h2><b>Climbing Journal</b></h2>
+		A place to keep nots on progress and more importantly<b>injuries</b> as suggested by MacLeod in
+		<i>Make or Break (pg. 22, 27)</i>
+		<ul class="list-disc px-6">
+			<li>Simple and open ended journaling tool; jot notes any day it seems useful</li>
+		</ul>
+	</li>
+</ul>
