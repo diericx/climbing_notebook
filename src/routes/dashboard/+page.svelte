f90036d81914d6c503f5d27384e14fa45c19104e
Zac Holland <zacharyholland@gmail.com>
2023-04-30 18:30:50 +0200

    dashboard cleanup

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import type { ProfileWithActiveTrainingProgram } from '$lib/prisma';
 	import type { CalendarEvent, JournalEntry } from '@prisma/client';
 	import Chart from '../chart/chart.svelte';
 	import Calendar from '../Calendar.svelte';
@@ -11,7 +10,7 @@
 	import { confirmDelete } from '$lib/utils';
 
 	export let data: PageData;
-	$: profile = data.profile as ProfileWithActiveTrainingProgram;
+	$: profile = data.profile;
 	$: journalEntries = data.journalEntries;
 	$: calendarEvents = data.calendarEvents;
 
@@ -35,99 +34,52 @@
 
 <br />
 
-{#if (data.user += undefined)}
-	<div class="pb-4">
-		<h1 class="inline">Your Goals</h1>
-		<a href={`/profile/edit?redirectTo=/`}>Edit</a>
+<div class="pb-4">
+	<h1 class="inline">Your Goals</h1>
+	<a href={`/profile/edit?redirectTo=/`}>Edit</a>
 
-		<hr />
-		{#if !profile.goals || profile.goals == ''}
-			<p class="text-gray-400">
-				You don't have any goals yet! Edit your <a href={`/profile/edit?redirectTo=/`}>Profile</a> set
-				some.
-			</p>
-		{:else}
-			<div class="whitespace-pre-wrap border bg-white w-full px-1 py-3">
-				{profile.goals}
-			</div>
-		{/if}
-	</div>
+	<hr />
+	{#if !profile.goals || profile.goals == ''}
+		<p class="text-gray-400">
+			You don't have any goals yet! Edit your <a href={`/profile/edit?redirectTo=/`}>Profile</a> set
+			some.
+		</p>
+	{:else}
+		<div class="whitespace-pre-wrap border bg-white w-full px-1 py-3">
+			{profile.goals}
+		</div>
+	{/if}
+</div>
+
+<div>
+	<h1>Calendar</h1>
+	<hr />
+
+	<button on:click={() => (showNewCalendarEventModal = true)}> New Event </button>
 
 	<div>
-		<h1>Calendar</h1>
-		<hr />
+		<Calendar
+			calendarEvents={calendarEvents || []}
+			journalEntries={journalEntries || []}
+			{onJournalEntryCalEventClick}
+			{onCalendarEventClick}
+		/>
+	</div>
+</div>
 
-		<button on:click={() => (showNewCalendarEventModal = true)}> New Event </button>
-
-		<div>
-			<Calendar
-				calendarEvents={calendarEvents || []}
-				journalEntries={journalEntries || []}
-				{onJournalEntryCalEventClick}
-				{onCalendarEventClick}
+<div>
+	<h1 class="inline">Charts</h1>
+	<a href="/chart/new?redirectTo=/">New Chart</a>
+	<hr />
+	<div>
+		{#each data.charts as chart}
+			<Chart
+				{chart}
+				targetObjects={chart.matchAgainst == 'metrics' ? data.metrics : data.exerciseEvents}
 			/>
-		</div>
+		{/each}
 	</div>
-
-	<div>
-		<h1 class="inline">Charts</h1>
-		<a href="/chart/new?redirectTo=/">New Chart</a>
-		<hr />
-		<div>
-			{#each data.charts as chart}
-				<Chart
-					{chart}
-					targetObjects={chart.matchAgainst == 'metrics' ? data.metrics : data.exerciseEvents}
-				/>
-			{/each}
-		</div>
-	</div>
-{:else}
-	<h1>Welcome to the Climbing Notebook</h1>
-	<hr />
-
-	<p>
-		<br />
-		This website is a no-fluff set of tools to help climbers with their training.
-		<br />
-		This is a <b>very early version</b> and is actively being worked on by myself only.
-		<br />
-		I am currently jobless and am willing to work on this nearly full time right now.
-		<br />
-		<br />
-		<b>If you want to help out</b> we can chat about what tooling you want added next.
-		<br />
-		zacharyholland[at]gmail[dot]com
-	</p>
-
-	<br />
-
-	<h1>Current Features</h1>
-	<hr />
-	<br />
-
-	<ul class="roman">
-		<li>
-			<h2><b>Climbing Journal</b></h2>
-			A place to keep nots on progress and more importantly<b>injuries</b> as suggested by MacLeod
-			in
-			<i>Make or Break (pg. 22, 27)</i>
-			<ul class="list-disc px-6">
-				<li>Simple and open ended journaling tool; jot notes any day it seems useful</li>
-			</ul>
-		</li>
-		<li>
-			<h2><b>Program Editor</b></h2>
-			Create complex training programs that will generate a weekly view.
-			<br />
-			Track completion of these tasks with the exercise log.
-		</li>
-		<li>
-			<h2><b>Exercise Log</b></h2>
-			Keep a detailed log of your exercises include repsxsets, times, and other notes.
-		</li>
-	</ul>
-{/if}
+</div>
 
 <Modal bind:showModal={showJournalEntryModal}>
 	<h2 class="font-bold">
