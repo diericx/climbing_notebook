5e5808e46f87f9ea09b0807f513fedf707d32436
Zac Holland <zacharyholland@gmail.com>
2023-05-02 12:03:02 +0200

    fix paths not relative

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['./src/**/*.{test,spec}.{js,ts}']
 	}
 };
 
