5ef2f7b62cba5a3e28234b19bf1b2431e929658e
Zac Holland <zacharyholland@gmail.com>
2023-05-07 12:59:43 +0200

    styled journal

--- src/routes/journalEntry/+page.svelte
+++ src/routes/journalEntry/+page.svelte
@@ -3,38 +3,54 @@
 	import FormJournalEntry from '$lib/components/forms/FormJournalEntry.svelte';
 	import { confirmDelete } from '$lib/utils';
 	import { enhance } from '$app/forms';
+	import Icon from '@iconify/svelte';
 
 	export let data: PageData;
 </script>
 
 <div class="grid grid-cols-1">
 	<div>
-		<h2>New Journal Entry</h2>
+		<h1>New Journal Entry</h1>
 		<hr />
 		<FormJournalEntry />
 	</div>
 </div>
 
 <div class="pt-8">
-	<h2>Your Entries</h2>
+	<h1>Entries</h1>
 	<hr />
 
 	<div>
 		{#each data.journalEntries as item}
-			<div class="flex justify-between">
-				<div class="w-full pb-5">
-					<h3 class="underline inline">{new Date(item.date).toLocaleDateString('en-US')}</h3>
-					<form
-						method="POST"
-						action={`/journalEntry/${item.id}/edit?/deleteJournalEntry`}
-						class="inline"
-						use:enhance
-					>
-						<input type="hidden" name="id" value={item.id} />
-						<button on:click={confirmDelete}>Delete</button>
-					</form>
-					<a href={`/journalEntry/${item.id}/edit?redirectTo=/journalEntry`}>Edit</a>
-					<p class="whitespace-pre-wrap bg-white w-full px-1 py-3">{item.content}</p>
+			<div class="card p-4 flex-1 mb-4">
+				<div class="flex w-full justify-between">
+					<div>
+						<h3 class="font-bold">{new Date(item.date).toLocaleDateString('en-US')}</h3>
+					</div>
+					<div>
+						<a
+							class="btn btn-sm variant-ringed"
+							href={`/journalEntry/${item.id}/edit?redirectTo=/journalEntry`}
+						>
+							<Icon icon="material-symbols:edit-outline" height="18" />
+							Edit
+						</a>
+						<form
+							method="POST"
+							action={`/journalEntry/${item.id}/edit?/deleteJournalEntry`}
+							class="inline"
+							use:enhance
+						>
+							<input type="hidden" name="id" value={item.id} />
+							<button class="btn btn-sm variant-ringed" on:click={confirmDelete}>
+								<Icon icon="mdi:trash-outline" height="18" />
+								Delete
+							</button>
+						</form>
+					</div>
+				</div>
+				<div>
+					<p class="whitespace-pre-wrap bg-white w-full py-2">{item.content}</p>
 				</div>
 			</div>
 		{/each}
