e4b57b8c10c9aaabc34f399295fc6cdc5d7dd022
Zac Holland <zacharyholland@gmail.com>
2023-06-12 11:50:56 +0200

    fix username case issues

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -19,7 +19,7 @@ export type LoginSchema = typeof loginSchema;
 
 export const signupSchema = z.object({
   email: z.string().email(),
-  username: z.string().min(1, { message: 'Username is required' }).refine(s => s.match(/^[0-9a-z]+$/), {
+  username: z.string().min(1, { message: 'Username is required' }).refine(s => s.match(/^[0-9a-zA-Z]+$/), {
     message: 'Username must contain only letters or numbers and no spaces.'
   }),
   password: z.string().min(5, { message: 'Password must be at least 5 characters long' }),
