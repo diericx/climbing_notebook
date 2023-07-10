e6fe3143a011a3498d7b27463e7127eb20db8d81
Zac Holland <zacharyholland@gmail.com>
2023-07-10 15:19:12 +0200

    added partial type to project

--- src/lib/project.ts
+++ src/lib/project.ts
@@ -9,40 +9,50 @@ export const projectSchema = z.object({
   huecoGrade: z.enum(huecoGrades).nullish(),
   gradeSystem: z.enum(gradeSystems),
   url: z.string().nullish(),
-  imageS3ObjectKey: z.string().nullish(),
+  imageS3ObjectKey: z.string().nullish()
 });
 export type ProjectSchema = typeof projectSchema;
+
+export const projectPartialSchema = z.object({
+  name: z.string().min(1).optional(),
+  fontGrade: z.enum(fontGrades).optional(),
+  huecoGrade: z.enum(huecoGrades).optional(),
+  gradeSystem: z.enum(gradeSystems).optional(),
+  url: z.string().nullish().optional(),
+  imageS3ObjectKey: z.string().optional()
+});
+export type ProjectPartialSchema = typeof projectPartialSchema;
 
 export const projectSessionSchema = z.object({
   notes: z.string().nullish(),
   date: z.date(),
-  sent: z.boolean().default(false),
+  sent: z.boolean().default(false)
 });
 export type ProjectSessionSchema = typeof projectSessionSchema;
 
 export class ProjectRepo {
-  constructor(private readonly prisma: PrismaClient) { }
+  constructor(private readonly prisma: PrismaClient) {}
 
   async getOneAndValidateOwner(id: string, ownerId: string) {
     const project = await this.prisma.project.findUnique({
       where: {
-        id: id,
+        id: id
       },
       include: {
         sessions: {
           orderBy: {
             date: 'desc'
           }
-        },
+        }
       }
     });
     if (project == null) {
       throw new APIError('NOT_FOUND', 'Resource not found');
     }
     if (project.ownerId != ownerId) {
-      throw new APIError('INVALID_PERMISSIONS', 'You do not have permission to edit this object.')
+      throw new APIError('INVALID_PERMISSIONS', 'You do not have permission to edit this object.');
     }
-    return project
+    return project;
   }
 
   async new(data: z.infer<ProjectSchema>, ownerId: string) {
@@ -50,7 +60,7 @@ export class ProjectRepo {
       data: {
         ...data,
         ownerId,
-        createdAt: new Date(),
+        createdAt: new Date()
       }
     });
   }
@@ -59,36 +69,36 @@ export class ProjectRepo {
     // Fetch all
     return await this.prisma.project.findMany({
       where: {
-        ownerId: ownerId,
+        ownerId: ownerId
       },
       orderBy: {
-        updatedAt: 'desc',
+        updatedAt: 'desc'
       },
       include: {
         sessions: {
           orderBy: {
             date: 'desc'
           }
-        },
+        }
       }
     });
   }
 
   async getOne(id: string, ownerId: string) {
-    return this.getOneAndValidateOwner(id, ownerId)
+    return this.getOneAndValidateOwner(id, ownerId);
   }
 
-  async update(data: z.infer<ProjectSchema>, id: string, ownerId: string) {
+  async update(data: z.infer<ProjectPartialSchema>, id: string, ownerId: string) {
     await this.getOneAndValidateOwner(id, ownerId);
 
     return await this.prisma.project.update({
       data: {
         ...data,
-        updatedAt: new Date(),
+        updatedAt: new Date()
       },
       where: {
-        id: id,
-      },
+        id: id
+      }
     });
   }
 
@@ -99,14 +109,14 @@ export class ProjectRepo {
       where: {
         id: id
       }
-    })
+    });
   }
 
   async addSession(data: z.infer<ProjectSessionSchema>, projectId: string, ownerId: string) {
     await this.getOneAndValidateOwner(projectId, ownerId);
     return await this.prisma.project.update({
       where: {
-        id: projectId,
+        id: projectId
       },
       data: {
         updatedAt: new Date(),
@@ -115,20 +125,20 @@ export class ProjectRepo {
             ...data,
             owner: {
               connect: {
-                id: ownerId,
+                id: ownerId
               }
             }
           }
         }
       }
-    })
+    });
   }
 
   async deleteSession(projectId: string, sessionId: string, ownerId: string) {
     await this.getOneAndValidateOwner(projectId, ownerId);
     return await this.prisma.project.update({
       where: {
-        id: projectId,
+        id: projectId
       },
       data: {
         updatedAt: new Date(),
@@ -136,14 +146,19 @@ export class ProjectRepo {
           delete: [{ id: sessionId }]
         }
       }
-    })
+    });
   }
 
-  async updateSession(data: z.infer<ProjectSessionSchema>, projectId: string, sessionId: string, ownerId: string) {
+  async updateSession(
+    data: z.infer<ProjectSessionSchema>,
+    projectId: string,
+    sessionId: string,
+    ownerId: string
+  ) {
     await this.getOneAndValidateOwner(projectId, ownerId);
     return await this.prisma.project.update({
       where: {
-        id: projectId,
+        id: projectId
       },
       data: {
         updatedAt: new Date(),
@@ -153,11 +168,11 @@ export class ProjectRepo {
               ...data
             },
             where: {
-              id: sessionId,
+              id: sessionId
             }
           }
         }
       }
-    })
+    });
   }
 }
