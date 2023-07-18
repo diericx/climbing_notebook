73ee2e6fd94265ff54544505b50a543a92d9cce3
Zac Holland <zacharyholland@gmail.com>
2023-07-18 13:06:32 +0200

    widget templates displayed in community widgets page

--- src/routes/widget/+page.server.ts
+++ src/routes/widget/+page.server.ts
@@ -6,6 +6,7 @@ import { SERVER_ERROR } from '$lib/helperTypes';
 import { superValidate } from 'sveltekit-superforms/server';
 import { WidgetRepo, widgetSchema } from '$lib/widget';
 import type { PageServerLoad } from './$types';
+import { CustomQueryRepo, type CustomQueryResults } from '$lib/customQuery';
 
 export const load: PageServerLoad = async ({ locals, url }) => {
   const { user } = await locals.auth.validateUser();
@@ -14,20 +15,43 @@ export const load: PageServerLoad = async ({ locals, url }) => {
   }
 
   const widgetRepo = new WidgetRepo(prisma);
+  const customQueryRepo = new CustomQueryRepo(prisma);
+
   let widgets;
   try {
     widgets = await widgetRepo.get({ isTemplate: true });
+    // compile datasets for widgets
+    const customQueryResults: CustomQueryResults[] = [];
+    for (const w of widgets) {
+      // Go through each widget and fetch cooresponding query results
+      if (w.type == 'chart' || w.type == 'heatmapCalendar') {
+        for (const dataset of w.datasets) {
+          for (const customQuery of dataset.customQueries) {
+            // Don't run the same queries multiple times
+            if (customQueryResults.find((r) => r.customQueryId == customQuery.id)) {
+              continue;
+            }
+
+            const data = await customQueryRepo.runCustomQuery(customQuery.id, user?.userId);
+            customQueryResults.push({
+              customQueryId: customQuery.id,
+              data,
+            });
+          }
+        }
+      }
+    }
+    return {
+      customQueryResults,
+      widgets,
+    };
   } catch (e) {
     if (e instanceof APIError) {
-      return fail(401, { message: e.detail });
+      throw error(401, { message: e.detail });
     }
     console.error(e);
     throw error(500, { message: SERVER_ERROR });
   }
-
-  return {
-    widgets,
-  };
 };
 
 export const actions: Actions = {
--- src/routes/widget/+page.svelte
+++ src/routes/widget/+page.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
+  import Widget from '$lib/components/Widget.svelte';
   import type { PageData } from './$types';
 
   export let data: PageData;
+  $: widgets = data.widgets;
+  $: customQueryResults = data.customQueryResults;
 </script>
 
-<div>
+<div class="mb-7">
   <h1>Community Widgets</h1>
+</div>
+
+<div class="mb-7">
+  <h3 class="text-gray-400">Created by me</h3>
+  <hr />
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+    {#each widgets as widget}
+      <div class={widget.width == 'full' ? 'col-span-2' : 'col-span-1'}>
+        <Widget {widget} {customQueryResults} />
+      </div>
+    {/each}
+  </div>
+</div>
+
+<div>
+  <hr />
 </div>
