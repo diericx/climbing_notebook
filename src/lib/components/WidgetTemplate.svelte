9a284165c9d2df530a40cfecc24f13e8a3e9ab39
Zac Holland <zacharyholland@gmail.com>
2023-07-19 12:50:42 +0200

    widgets are clickable

--- src/lib/components/WidgetTemplate.svelte
+++ src/lib/components/WidgetTemplate.svelte
@@ -15,49 +15,51 @@
 </script>
 
 <div class="block card card-hover p-4">
-  <div class="w-full flex mb-1">
-    <div class="flex-1">
-      <div class="font-bold text-xl">
-        {widget.name}
+  <a style="height: 100%;" class="flex flex-col justify-between" href={`/widget/${widget.id}`}>
+    <div class="w-full flex mb-1">
+      <div class="flex-1">
+        <div class="font-bold text-xl">
+          {widget.name}
+        </div>
+      </div>
+      {#if widget.owner.id == user.userId}
+        <div>
+          <a class="btn btn-sm variant-ringed mr-2" href={`/widget/${widget.id}/edit`}>
+            <Icon icon="material-symbols:edit-outline" height="18" />
+            <span>Edit</span>
+          </a>
+        </div>
+        <form method="POST" action={`/widget/${widget.id}?/delete`} use:enhance>
+          <input type="hidden" name="id" value={widget.id} />
+          <button class="btn btn-sm variant-ringed" on:click={confirmDelete}>
+            <Icon icon="mdi:trash-outline" height="18" />
+            Delete
+          </button>
+        </form>
+      {/if}
+    </div>
+    <div class="mb-2">
+      <div class="text-gray-500">
+        {widget.description}
+        <br />
       </div>
     </div>
-    {#if widget.owner.id == user.userId}
-      <div>
-        <a class="btn btn-sm variant-ringed mr-2" href={`/widget/${widget.id}/edit`}>
-          <Icon icon="material-symbols:edit-outline" height="18" />
-          <span>Edit</span>
-        </a>
-      </div>
-      <form method="POST" action={`/widget/${widget.id}?/delete`} use:enhance>
-        <input type="hidden" name="id" value={widget.id} />
-        <button class="btn btn-sm variant-ringed" on:click={confirmDelete}>
-          <Icon icon="mdi:trash-outline" height="18" />
-          Delete
-        </button>
-      </form>
-    {/if}
-  </div>
-  <div class="mb-2">
-    <div class="text-gray-500">
-      {widget.description}
-      <br />
+    <div class="mb-4 text-gray-500">
+      by <b>{widget.owner.username}</b>
     </div>
-  </div>
-  <div class="mb-4 text-gray-500">
-    by <b>{widget.owner.username}</b>
-  </div>
 
-  {#if widget.type == 'chart'}
-    {#if widget.datasets.length == 0}
-      <p class="text-gray-400 italic">
-        Widget is not fully configured yet. Edit the widget to finish configuration.
-      </p>
-    {:else}
-      <Chart datasets={widget.datasets} {customQueryResults} />
+    {#if widget.type == 'chart'}
+      {#if widget.datasets.length == 0}
+        <p class="text-gray-400 italic">
+          Widget is not fully configured yet. Edit the widget to finish configuration.
+        </p>
+      {:else}
+        <Chart datasets={widget.datasets} {customQueryResults} />
+      {/if}
+    {:else if widget.type == 'heatmapCalendar'}
+      <div>
+        <HeatmapCalendar datasets={widget.datasets} {customQueryResults} />
+      </div>
     {/if}
-  {:else if widget.type == 'heatmapCalendar'}
-    <div>
-      <HeatmapCalendar datasets={widget.datasets} {customQueryResults} />
-    </div>
-  {/if}
+  </a>
 </div>
