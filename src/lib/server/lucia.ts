fde926ca53891adb9ce0874e93af433f55bff1cf
Zac Holland <zacharyholland@gmail.com>
2023-08-17 18:31:57 +0200

    update prisma adapter client usage on a hunch

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -1,12 +1,13 @@
 // lib/server/lucia.ts
 import lucia from 'lucia-auth';
 import { sveltekit } from 'lucia-auth/middleware';
-import prisma from '@lucia-auth/adapter-prisma';
+import prismaAdapter from '@lucia-auth/adapter-prisma';
 import { dev } from '$app/environment';
 import { PrismaClient } from '@prisma/client';
+import { prisma } from '$lib/prisma';
 
 export const auth = lucia({
-  adapter: prisma(new PrismaClient()),
+  adapter: prismaAdapter(prisma),
   env: dev ? 'DEV' : 'PROD',
   transformDatabaseUser: (userData) => {
     return {
