3663be469bcfdb3ebeef0f62ca14a9b911105b43
Zac Holland <zacharyholland@gmail.com>
2023-08-19 09:16:42 +0200

    remove auth logic from hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -11,24 +11,5 @@ if (!dev) {
 
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.auth = auth.handleRequest(event);
-  const session = await event.locals.auth.validate();
-
-  // TODO: remove all this logic as it will be in the actions and loads
-  if (
-    event.url.pathname.startsWith('/chart') ||
-    event.url.pathname.startsWith('/exerciseEvent') ||
-    event.url.pathname.startsWith('/journalEntry') ||
-    event.url.pathname.startsWith('/profile') ||
-    event.url.pathname.startsWith('/query') ||
-    event.url.pathname.startsWith('/dashboard') ||
-    event.url.pathname.startsWith('/project') ||
-    event.url.pathname.startsWith('/exercise') ||
-    event.url.pathname.startsWith('/widget')
-    // trainingProgram is handled at the path level
-  ) {
-    await getSessionOrRedirect({ locals: event.locals, url: event.url });
-  }
-
-  const response = await resolve(event);
-  return response;
+  return await resolve(event);
 };
