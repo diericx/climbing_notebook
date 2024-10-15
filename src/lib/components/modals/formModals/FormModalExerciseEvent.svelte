ae3c4705c45d76991ea22af2bdfae67e614733cf
Zac Holland <zacharyholland@gmail.com>
2024-10-14 17:19:48 -0700

    modals updated to invalidate queries

--- src/lib/components/modals/formModals/FormModalExerciseEvent.svelte
+++ src/lib/components/modals/formModals/FormModalExerciseEvent.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   import { exerciseEventSchema } from '$lib/zodSchemas';
+  import { useQueryClient } from '@sveltestack/svelte-query';
   import FormBodyExerciseEvent from '../../forms/bodies/FormBodyExerciseEvent.svelte';
   import FormModal from './FormModal.svelte';
+  const queryClient = useQueryClient();
 </script>
 
-<FormModal schema={exerciseEventSchema} let:superForm let:formProps>
+<FormModal
+  schema={exerciseEventSchema}
+  let:superForm
+  let:formProps
+  onSuccess={() => queryClient.invalidateQueries('exerciseEvents')}
+>
   <FormBodyExerciseEvent {superForm} {...formProps} showSubmitButton={false} />
 </FormModal>
