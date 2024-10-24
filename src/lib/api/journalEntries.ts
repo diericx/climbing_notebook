3fa69b854c8d4c0961f9b4311d24605daafe2113
Zac Holland <zacharyholland@gmail.com>
2024-10-24 10:07:14 -0700

    Fix `Failed to parse URL from ...` error (#66)

--- src/lib/api/journalEntries.ts
+++ src/lib/api/journalEntries.ts
@@ -1,8 +1,9 @@
+import { APIBaseURL } from '$lib/utils';
 import { useQuery } from '@sveltestack/svelte-query';
 
 export function getJournalEntries(startStr: string, endStr: string) {
   const fetchData = async () => {
-    const data = await fetch(`/api/journalEntries?start=${startStr}&end=${endStr}`);
+    const data = await fetch(`${APIBaseURL()}/api/journalEntries?start=${startStr}&end=${endStr}`);
     return data.json();
   };
 
